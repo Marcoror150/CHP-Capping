be9b71df17d118c65f95aab59a4fd72eaecedc46
Michael Gutierrez <michael.gutierrez2@marist.edu>
2018-10-10 16:07:48 -0400

    Python script to parse csv files to create tables with entries

---
+++ Backend/csv_to_db.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+from db_helper import *
+import os
+import re 
+
+
+# Cleanse input from whitespace and commas
+def cleanse(input):
+    output = input.replace(' ', '')
+    output = output.replace(',', '')
+    output = output.replace('-', '')
+    output = output.replace('\r', '')
+    output = output.replace('\n', '')
+
+    return output
+
+# Cleanses elements in a list
+def cleanseMany(input):
+    for i, item in enumerate(input):
+        input[i] = cleanse(item)
+    return input
+
+# Strip all characters that are not numbers or decimal
+def cleanseNonNumbers(input):
+    regex = re.compile(r'[^0-9^.,]')
+    return regex.sub('', input)
+
+# Parse data to be ready for insertion
+def parseData(raw_data):
+    data = []
+    # Iterate the raw data to cleanse it, and parse out appropiate data types
+    for i, item in enumerate(raw_data):
+        raw_data[i] = cleanseNonNumbers(item)
+        data_split = raw_data[i].split(',') 
+        print(data_split)
+        col1 = int(data_split[0])
+        col2 = float(data_split[1])
+        data.append([col1,col2])
+    return data
+
+# Converts all csv files into tables into the db
+def main():
+    os.chdir('csvs')
+    files = os.listdir('.')
+
+    # Loop through every csv file, create a table and insert data
+    for file in files:
+        if file.endswith('.csv'):
+            f = open(file,'r')
+            table = cleanse(f.readline().split(',')[0])
+            columns = cleanseMany(f.readline().split(','))
+            data = parseData(f.readlines())
+            f.close()
+
+            createTable(table,columns)
+            insertTable(table,columns,data)
+    
+    showAllTables()    
+if __name__ == "__main__":
+    main()
---
+++ Backend/db_helper.py
@@ -0,0 +1,115 @@
+#!/usr/bin/env python3
+import pyodbc
+
+# Connect to the db and return the connection and cursor for query execution
+def connectToDB():
+    try:
+        # Connect to the db and capture its object
+        conn = pyodbc.connect('DSN=pochildrenshome;Trusted_Connection=yes;')
+        conn.setencoding('utf-8') 
+
+        # Create a cursor from the connection object
+        cur = conn.cursor()
+        return conn, cur
+    except:
+        raise ValueError('Unable to connect to the database')
+
+# Create a new table given a table name and the necessary columns
+def createTable(table,columns):
+    conn, cur = connectToDB()
+
+    test1 = "dummy_table"
+    test2 = ["Month","Means"]
+    test3 = [[0.12,0.22],[0.3,0.5]]
+
+    sql = """
+        CREATE TABLE %s ( 
+            %s int          NOT NULL,
+            %s decimal(3,2) NOT NULL
+        );
+    """
+    # Stitch together the function arguements to the sql statement
+    sql = sql % (table, columns[0], columns[1])
+
+    # Execute the query, commit changes and close the connection
+    try:
+        cur.execute(sql)
+        conn.commit()
+    except:
+        conn.close()
+        print('Unable to create table ' + table+ ' check if exists')
+    
+# Insert new entries into an existing table
+def insertTable(table,columns,values):
+    conn, cur = connectToDB()
+    sql = """
+        INSERT INTO %s (%s,%s) values(?,?);
+    """
+
+    # Stitch together the function arguements to the sql statement
+    sql = sql % (table, columns[0], columns[1])
+
+    # Execute the query, commit changes and close the connection
+    try:
+        cur.executemany(sql, values)
+        conn.commit()
+        conn.close()
+    except:
+        conn.close()
+        raise ValueError('Unable to insert into table ' + table)
+
+# Get all entries of a given table
+def selectAll(table):
+    conn, cur = connectToDB()
+    sql = """
+        SELECT * FROM dbo.%s;
+    """
+    # Stitch together the table to the sql statement
+    sql = sql % (table)
+
+    # Execute the query and close the connection
+    try:
+        cur.execute(sql)
+
+        # Iterate through the cursor results if there are any
+        entries = cur.fetchall()
+        if not entries:
+            print('No entries')
+            return
+        # Print all entries to check
+        for entry in entries:
+            print(entry)
+        conn.close()
+
+    except:
+        conn.close()
+        raise ValueError('Table ' + table + ' does not exist')
+
+# Get all tables in the db
+def showAllTables():
+    conn, cur = connectToDB()
+    sql = """
+        SELECT SYSSCHEMA.NAME, SYSTABLE.NAME
+        FROM SYS.tables SYSTABLE
+        INNER JOIN SYS.SCHEMAS SYSSCHEMA
+        ON SYSTABLE.SCHEMA_ID = SYSSCHEMA.SCHEMA_ID;
+    """
+    # Execute the query and close the connection
+    try:
+        cur.execute(sql)
+        # Iterate through the cursor results if there are any
+        entries = cur.fetchall()
+        if not entries:
+            print('No entries')
+            return
+        # Print all entries to check
+        for entry in entries:
+            print(entry)
+        conn.close()
+    except:
+            conn.close()
+
+
+
+
+
