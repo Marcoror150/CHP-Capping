d7c7714f62baa76a19f5005aa362d0cff2453196
Marc Christensen <marc.christensen1@marist.edu>
2018-10-10 22:24:20 -0400

    Fixed style.

--- Backend/server.py
+++ Backend/server.py
@@ -2,10 +2,8 @@ from flask import Flask
 from flask import request
 from flask import render_template
 
-
 app = Flask(__name__)
 app.debug = True
-
 
 @app.route('/', methods=['GET', 'POST'])
 def login():
