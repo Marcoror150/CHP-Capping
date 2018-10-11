5df60f63c6b9ff8babe064065a8c0330df49882f
Michael Gutierrez <michael.gutierrez2@marist.edu>
2018-10-10 21:06:17 -0400

    Added some routes for Flask server

--- Backend/server.py
+++ Backend/server.py
@@ -1,9 +1,22 @@
 from flask import Flask
-app = Flask(__name__)
+from flask import request
+from flask import render_template
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+
+app = Flask(__name__)
+app.debug = True
+
+
+@app.route('/', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        return do_the_login()
+    else:
+      return render_template('index.html')
+
+@app.route("/charts", methods=['GET'])
+def charts():
+    return render_template('Charts.html')
     
 if __name__ == "__main__":
     app.run()
