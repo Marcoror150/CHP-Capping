f4ad6f151b21797b310ff8351218f0d83256e98b
Marc Christensen <marc.christensen1@marist.edu>
2018-10-21 13:55:28 -0400

    Working on creating all the directories.

--- Scripts/fileAutomation.sh
+++ Scripts/fileAutomation.sh
@@ -7,7 +7,7 @@
 
 RTC='./rtc'
 YMP='./ymp'
-SHP='.shp'
+SHP='./shp'
 ABH='./abh'
 GEFC='./gefc'
 
@@ -17,5 +17,51 @@ then
 else
   echo "$RTC does not exist in this directory. Creating the directory now."
   mkdir $RTC
+  cd $RTC
+  mkdir Archive
+  cd ../../
 fi
 
+if [ -e $YMP ]
+then
+  echo "$YMP exists"
+else
+  echo "$YMP does not exist in this directory. Creating the directory now."
+  mkdir $YMP
+  cd $YMP
+  mkdir Archive
+  cd ../../
+fi
+
+if [ -e $SHP ]
+then
+  echo "$SHP exists"
+else
+  echo "$SHP does not exist in this directory. Creating the directory now."
+  mkdir $SHP
+  cd $SHP
+  mkdir Archive
+  cd ../../
+fi
+
+if [ -e $ABH ]
+then
+  echo "$ABH exists"
+else
+  echo "$ABH does not exist in this directory. Creating the directory now."
+  mkdir $ABH
+  cd $ABH
+  mkdir Archive
+  cd ../../
+fi
+
+if [ -e $GEFC ]
+then
+  echo "$GEFC exists"
+else
+  echo "$GEFC does not exist in this directory. Creating the directory now."
+  mkdir $GEFC
+  cd $GEFC
+  mkdir Archive
+  cd ../../
+fi
