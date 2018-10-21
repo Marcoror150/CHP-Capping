ef07e52a42a7db70f9df43384e79e5385ca86cef
Marc Christensen <marc.christensen1@marist.edu>
2018-10-21 13:48:38 -0400

    Added variables for all the residential programs and basic if staement to create the folders.

--- Scripts/fileAutomation.sh
+++ Scripts/fileAutomation.sh
@@ -1,7 +1,21 @@
+#!/bin/bash
+#
 # Bash Script file used to automate the processing of info from incidents,
 # then moving the processed files to an Archive folder.
 # Author: Marc Christensen
 ###
 
-#!/bin/bash
+RTC='./rtc'
+YMP='./ymp'
+SHP='.shp'
+ABH='./abh'
+GEFC='./gefc'
+
+if [ -e $RTC ]
+then
+  echo "$RTC exists"
+else
+  echo "$RTC does not exist in this directory. Creating the directory now."
+  mkdir $RTC
+fi
 
