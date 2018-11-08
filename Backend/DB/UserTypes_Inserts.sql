02bffa366fcd801f58ef87b30c060ab36e564b10
Marc Christensen <marc.christensen1@marist.edu>
2018-11-07 20:31:20 -0500

    Tweaked user database workding.

--- Backend/DB/UserTypes_Inserts.sql
+++ Backend/DB/UserTypes_Inserts.sql
@@ -6,19 +6,19 @@ Tested on MS SQL 2017
 */
 
 INSERT INTO UserTypes (UserType, Description)
-VALUES ('root', 'Root user, complete control.');
+VALUES ('Root', 'Root user, complete control.');
 
 INSERT INTO UserTypes (UserType, Description)
-VALUES ('admin', 'Manage accounts and database.');
+VALUES ('Admin', 'Manage accounts and database.');
 
 INSERT INTO UserTypes (UserType, Description)
-VALUES ('highUser', 'Can insert/delete incidents to database and can create/remove reports.');
+VALUES ('Full User', 'Can insert/delete incidents to database and can create/remove reports.');
 
 INSERT INTO UserTypes (UserType, Description)
-VALUES ('superIntern', 'Can insert incidents to database and can create reports.');
+VALUES ('Super Intern', 'Can insert incidents to database and can create reports.');
 
 INSERT INTO UserTypes (UserType, Description)
-VALUES ('intern', 'Suggests poetential incidents and can create reports.');
+VALUES ('Intern', 'Suggests poetential incidents and can create reports.');
 
 INSERT INTO UserTypes (UserType, Description)
-VALUES ('viewOnly', 'User can generate and view reports only.');
+VALUES ('View Only', 'User can generate and view reports only.');
--- Backend/DB/Users_Inserts.sql
+++ Backend/DB/Users_Inserts.sql
@@ -6,19 +6,19 @@ Tested on MS SQL 2017
 */
 
 INSERT INTO Users (Username, Password, First_Name, Last_Name, UserType)
-VALUES ('root', 'root', 'root', 'root', 'root');
+VALUES ('root', 'root', 'root', 'root', 'Root');
 
 INSERT INTO Users (Username, Password, First_Name, Last_Name, UserType)
-VALUES ('don_marie', 'password', 'Donnamarie', 'LastName', 'highUser');
+VALUES ('don_marie', 'password', 'Donnamarie', 'LastName', 'Full User');
 
 INSERT INTO Users (Username, Password, First_Name, Last_Name, UserType)
-VALUES ('drC', 'password', 'dr', 'c', 'viewOnly');
+VALUES ('drC', 'password', 'dr', 'c', 'View Only');
 
 INSERT INTO Users (Username, Password, First_Name, Last_Name, UserType)
-VALUES ('Phillip', 'admin', 'Phillip', 'admin', 'admin');
+VALUES ('Phillip', 'admin', 'Phillip', 'admin', 'Admin');
 
 INSERT INTO Users (Username, Password, First_Name, Last_Name, UserType)
-VALUES ('s_intern', 'password', 'super', 'intern', 'superIntern');
+VALUES ('s_intern', 'password', 'super', 'intern', 'Super Intern');
 
 INSERT INTO Users (Username, Password, First_Name, Last_Name, UserType)
-VALUES ('intern1', 'password', 'intern', 'intern', 'intern');
+VALUES ('intern1', 'password', 'intern', 'intern', 'Intern');
