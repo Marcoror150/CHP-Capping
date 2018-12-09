f416ac666aed98de6189d8781dfbc80ff693a6ab
Van Allen <brendan.vanallen@CHP.com>
2018-12-09 14:38:15 -0500

    Super Intern can now remove reports

--- Backend/server.py
+++ Backend/server.py
@@ -439,7 +439,7 @@ def deleteReport(GID):
     # Prevent unauthorized access to this page via URL manipulation
     if not session.get('userType'):
         return redirect('')
-    elif session['userType'] in ['Intern','Super Intern']:
+    elif session['userType'] == 'Intern':
         return redirect('/recordupload')
     elif session['userType'] == 'View Only':
         return redirect('/recordupload')
